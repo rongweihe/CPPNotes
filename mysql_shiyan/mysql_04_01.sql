524cb6c0934dc4a5f031325027f6cf2075514dc9
rongweihe <rongweihe1995@126.com>
2020-05-24 20:59:05 +0800

    add sql

---
+++ mysql_shiyan/mysql_04_01.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE mysql_shiyan;
+
+use mysql_shiyan;
+
+CREATE TABLE department
+(
+  dpt_name   CHAR(20) NOT NULL,
+  people_num INT(10) DEFAULT '10',
+  CONSTRAINT dpt_pk PRIMARY KEY (dpt_name)
+ );
+
+CREATE TABLE employee
+(
+  id      INT(10) PRIMARY KEY,
+  name    CHAR(20),
+  age     INT(10),
+  salary  INT(10) NOT NULL,
+  phone   INT(12) NOT NULL,
+  in_dpt  CHAR(20) NOT NULL,
+  UNIQUE  (phone),
+  CONSTRAINT emp_fk FOREIGN KEY (in_dpt) REFERENCES department(dpt_name)
+ );
+ 
+CREATE TABLE project
+(
+  proj_num   INT(10) NOT NULL,
+  proj_name  CHAR(20) NOT NULL,
+  start_date DATE NOT NULL,
+  end_date   DATE DEFAULT '2020-12-30',
+  of_dpt     CHAR(20) REFERENCES department(dpt_name),
+  CONSTRAINT proj_pk PRIMARY KEY (proj_num,proj_name)
+ );
